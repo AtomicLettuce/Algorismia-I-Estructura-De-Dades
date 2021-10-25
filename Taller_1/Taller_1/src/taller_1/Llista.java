bd694518f18baa429e9955871fb06537b334455a
AtomicLettuce <xvivesm@gmail.com>
2021-10-25 18:09:32 +0200

    final

--- Taller_1/Taller_1/src/taller_1/Llista.java
+++ Taller_1/Taller_1/src/taller_1/Llista.java
@@ -65,7 +65,7 @@ public class Llista {
     // Mètode que retorna el node situat a la posició passada per paràmetre
     public Node getNode(int pos) {
         Node aux = primer;
-        for (int i = 0; i < pos; i++) {
+        for (int i = 0; i < pos-1; i++) {
             if (aux != null) {
                 aux = aux.getSeg();
             }
@@ -76,10 +76,30 @@ public class Llista {
     // Mètode que elimina el node situat a la posició passada per paràmetre
     public void eliminarNode(int pos) {
         Node aux = primer;
-        Node aux2 = null;
+        Node aux2 = primer;
+
+        if (pos == 1) {
+            primer=primer.getSeg();
+        } else {
+
+            for (int i = 1; i < pos; i++) {
+                aux2 = aux;
+                aux = aux.getSeg();
+            }
+
+            if (aux == null) {
+                aux2.setSeg(null);
+            } else if (aux.getSeg() != null) {
+                aux2.setSeg(aux.getSeg());
+            }else{
+                aux2.setSeg(null);
+            }
+        }
+
+        /*
         
         // Cercam el node que està a la posicio indicada
-        for (int i = 0; i < pos-1; i++) {
+        for (int i = 0; i < pos - 1; i++) {
             if (aux != null) {
                 aux2 = aux;
                 aux = aux.getSeg();
@@ -88,10 +108,9 @@ public class Llista {
         // Feim el canvi de punters si és que existeix tal posició
         if (aux != null) {
             aux2.setSeg(aux.getSeg());
-        }else{
+        } else {
             System.out.println("No existeix tal posicio");
-        }
-
+        }*/
     }
 
     // Mètode que imprimeix per pantalla tot el contingut de la llista
@@ -105,5 +124,39 @@ public class Llista {
             posicio++;
         }
         System.out.println("-----Final de llista-----");
+    }
+
+    public void afegirNode(Node nounode) {
+        //Utilitzarem dos nodes auxiliars i una variable booleana per sabre si es troba col·locat
+        Node aux = primer;
+        Node aux2 = null;
+        boolean colocado = false;
+
+        //si és més petit que el primer node, es coloca al inici de la llista 
+        //colocat a true
+        if (nounode.getInfo() < aux.getInfo()) {
+            afegirInici(nounode);
+            colocado = true;
+        } else {
+            //sino es troba al principi recorregut de la llista comparant    
+            while (aux != null) {
+                //seleccionam node llista dins aux2 i seleccionam el següent a aux
+                aux2 = aux;
+                aux = aux.getSeg();
+                //Comparació entre aux2, nounode i el següent de la llista
+                if ((aux2.getInfo() < nounode.getInfo()) && (nounode.getInfo() < aux.getInfo())) {
+                    //Col·locam node dins llista, colocat a true i aux= null per sortir del bucle
+                    aux2.setSeg(nounode);
+                    nounode.setSeg(aux);
+                    colocado = true;
+                    aux = null;
+                }
+
+            }
+        }
+        //Si es més gran que la resta de nombres de la llista, no col·locat, posam al final
+        if (!colocado) {
+            afegirFinal(nounode);
+        }
     }
 }
