44f66d4f1683f1fd8878badcdc111b7f89b731ca
Atomic_Lettuce <xvivesm@gmail.com>
2021-12-20 16:56:10 +0100

    problema n reines2

--- Practica2/Practica2/src/practica2/Practica2.java
+++ Practica2/Practica2/src/practica2/Practica2.java
@@ -4,8 +4,6 @@ import java.awt.BorderLayout;
 import java.awt.Color;
 import static java.awt.Color.*;
 import java.awt.Container;
-import java.awt.Graphics;
-import java.awt.Graphics2D;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
